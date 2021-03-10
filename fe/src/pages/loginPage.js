ef397d1a546bf629e1559dc7359f660bc4b440cb
roberteftene <roberteftene25@gmail.com>
2021-03-10 15:56:13 +0200

    implemented auth services

---
+++ fe/src/pages/loginPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Form } from "react-bootstrap";
+
+
+export default class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+            </div>
+        )
+    }
+}
---
+++ fe/src/services/auth/auth.service.js
@@ -0,0 +1,42 @@
+import axios from 'axios'
+
+const API_URL = 'http://localhost:8080/api/auth'
+const API_AUTH_URL = process.env.REACT_APP_API_AUTH_URL;
+
+class AuthService {
+
+    login(username, password) {
+        return axios.post(
+            API_AUTH_URL + 'signin', {
+                username,
+                password
+            }
+        ).then(res => {
+            if(res.data.accessToken) {
+                localStorage.setItem("user", JSON.stringify(res.data))
+            }
+            return res.data;
+        })
+    }
+
+    logout() {
+        localStorage.removeItem('user');
+    }
+
+    register(username, email, password) {
+        return axios.post(
+            API_AUTH_URL + 'signup', {
+                username,
+                email,
+                password
+            }
+        )
+    }
+
+    getLoggedUser() {
+        return JSON.parse(localStorage.getItem('user'))
+    }
+
+}
+
+export default new AuthService();
